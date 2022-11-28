4c2810f73258f18c1d6080aeb44aa0140fb58178
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-28 16:08:00 +0530

    readvalues

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent {
-
+pTitle=""
+pPrice=""
+pCategory=""
+pDescription=""
+pRating=""
+readValue=()=>{
+let data:any={"pTitle":this.pTitle,"pPrice":this.pPrice,"pCategory":this.pCategory,"pDescription":this.pDescription,"pRating":this.pRating}
+console.log(data)
+}
 }
